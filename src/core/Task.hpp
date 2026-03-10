fb2736e938f5188cacb1e85a27e26eb2e1c9e7a3
Anna <andeftones@gmail.com>
2026-03-10 21:56:40 +0300

    add Task abstract class

---
+++ src/core/Task.hpp
@@ -0,0 +1,24 @@
+#ifndef TASK_HPP
+#define TASK_HPP
+#include <string>
+class Task {
+protected:
+    int id_;
+    std::string title_;
+    bool complete_;
+
+public:
+    Task(int id, const std::string& title);
+    virtual ~Task() = default;
+
+    int getId() const;
+    const std::string& getTitle() const;
+    bool isComplete() const;
+
+    void markDone();
+    void markUndone();
+
+    virtual std::string getType() const = 0;
+    virtual std::string toString() const = 0;
+};
+#endif
