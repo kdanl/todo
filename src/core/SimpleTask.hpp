8efe7884f33703b852e1977b381bf607dd3e0752
Anna <andeftones@gmail.com>
2026-03-11 01:28:23 +0300

    add derived class SimpleTask

---
+++ src/core/SimpleTask.hpp
@@ -0,0 +1,10 @@
+#ifndef SIMPLETASK_HPP
+#define SIMPLETASK_HPP
+#include "Task.hpp"
+class SimpleTask : public Task{
+public:
+    SimpleTask(int id,const std::string& title);
+    std::string getType() const override;
+    std::string toString() const override;
+};
+#endif
