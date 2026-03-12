bb309315e985ba76789a949a5a6039d71ac6d586
Anna <andeftones@gmail.com>
2026-03-13 02:05:44 +0300

    implement priority support

--- src/core/Task.cpp
+++ src/core/Task.cpp
@@ -1,6 +1,6 @@
 #include "Task.hpp"
 
-Task::Task(int id, const std::string& title) : id_(id),title_(title), complete_(false){}
+Task::Task(int id, const std::string& title, int priority) : id_(id),title_(title), complete_(false),priority_(priority){}
 
 int Task::getId() const {
     return id_;
@@ -20,4 +20,11 @@ void Task::markUndone() {
 }
 void Task::setTitle(const std::string &title) {
     title_ = title;
+}
+int Task::getPriority() const {
+    return priority_;
+}
+
+void Task::SetPriority(int newPriority) {
+    priority_ = newPriority;
 }
