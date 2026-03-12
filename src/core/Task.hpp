40876648cf1e40236fb441755cfc34940314cfc7
Anna <andeftones@gmail.com>
2026-03-13 02:39:04 +0300

    replace integer priority with enum class Priority in Task

--- src/core/Task.hpp
+++ src/core/Task.hpp
@@ -1,27 +1,35 @@
 #ifndef TASK_HPP
 #define TASK_HPP
 #include <string>
+enum class Priority {
+    Low,
+    Medium,
+    High
+};
+std::string priorityToString(Priority p);
+Priority priorityFromString(const std::string& str);
 class Task {
 protected:
     int id_;
     std::string title_;
     bool complete_;
-    int priority_;
+    Priority priority_;
 
 public:
-    Task(int id, const std::string& title,int priority);
+    Task(int id, const std::string& title, Priority priority);
     virtual ~Task() = default;
 
     int getId() const;
     const std::string& getTitle() const;
     bool isComplete() const;
-    int getPriority() const;
+    Priority getPriority() const;
 
     void markDone();
     void markUndone();
     void setTitle(const std::string& title);
-    void SetPriority(int newPriority);
+    void setPriority(Priority newPriority);
     virtual std::string getType() const = 0;
     virtual std::string toString() const = 0;
 };
+
 #endif
