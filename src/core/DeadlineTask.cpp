9dd48b96725bf809a0ac74aad8617bf73fb77011
малина <malina@MacBook-Pro-malina.local>
2026-03-12 15:08:55 +0300

    Add DeadlineTask implementation and update header

---
+++ src/core/DeadlineTask.cpp
@@ -0,0 +1,17 @@
+#include "DeadlineTask.hpp"
+
+DeadlineTask::DeadlineTask(int id, const std::string &title, const std::string &deadline):Task(id,title),deadline_(deadline) {
+}
+const std::string& DeadlineTask::getDeadline() const {
+    return deadline_;
+}
+void DeadlineTask::setDeadline(const std::string& deadline) {
+    deadline_ = deadline;
+}
+std::string DeadlineTask::getType() const {
+    return "deadline";
+}
+std::string DeadlineTask::toString() const {
+    std::string status = complete_ ? "[✓] " : "[ ] ";
+    return status + getTitle() + " (deadline: " + deadline_ + ")";
+}
