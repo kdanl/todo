9646239c5aaf5ed88a6b8ab98d717b8ed607e73c
Anna <andeftones@gmail.com>
2026-03-13 01:21:26 +0300

    add Priority

--- src/core/Task.hpp
+++ src/core/Task.hpp
@@ -6,14 +6,16 @@ protected:
     int id_;
     std::string title_;
     bool complete_;
+    int priority_;
 
 public:
-    Task(int id, const std::string& title);
+    Task(int id, const std::string& title,int priority);
     virtual ~Task() = default;
 
     int getId() const;
     const std::string& getTitle() const;
     bool isComplete() const;
+    int getPriority() const;
 
     void markDone();
     void markUndone();
@@ -21,5 +23,6 @@ public:
     virtual std::string getType() const = 0;
     virtual std::string toString() const = 0;
     void setTitle(const std::string& title);
+    void SetPriority(int newPriority);
 };
 #endif
