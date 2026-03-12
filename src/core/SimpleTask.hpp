240429385a2952cba447b1fa91ab624b7d15f2bc
Anna <andeftones@gmail.com>
2026-03-13 02:13:07 +0300

    fix constructor

--- src/core/SimpleTask.hpp
+++ src/core/SimpleTask.hpp
@@ -1,10 +1,11 @@
 #ifndef SIMPLETASK_HPP
 #define SIMPLETASK_HPP
 #include "Task.hpp"
-class SimpleTask : public Task{
+class SimpleTask : public Task {
 public:
-    SimpleTask(int id,const std::string& title);
+    SimpleTask(int id, const std::string& title, int priority);
     std::string getType() const override;
     std::string toString() const override;
 };
+
 #endif
