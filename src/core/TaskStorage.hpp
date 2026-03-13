c1f73feb9572fac9c19af7d7da17ae296bcb5f65
Anna <andeftones@gmail.com>
2026-03-13 13:09:57 +0300

    add updateRepeatingTasks

--- src/core/TaskStorage.hpp
+++ src/core/TaskStorage.hpp
@@ -1,21 +1,35 @@
 #ifndef TASKSTORAGE_HPP
 #define TASKSTORAGE_HPP
+
 #include <memory>
 #include <vector>
 #include <optional>
 #include "Task.hpp"
+
 class TaskStorage{
 private:
     std::vector<std::unique_ptr<Task>> tasks_;
+
 public:
     void addTask(std::unique_ptr<Task> task);
+
     Task* findById(int id) const;
+
     bool removeTask(int id);
+
     const std::vector<std::unique_ptr<Task>>& getTasks() const;
+
     std::vector<Task*> findByTitle(const std::string& keyword) const;
+
     int getTaskCount() const;
+
     int getCompletedCount() const;
-    std::optional<int> getProgressPercentage()const;
+
+    std::optional<int> getProgressPercentage() const;
+
     void sortByPriority();
+
+    void updateRepeatingTasks();
 };
+
 #endif
