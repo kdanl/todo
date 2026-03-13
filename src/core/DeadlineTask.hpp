afdbe21d67ad7060b8d07aff75dbe34b807b2aa7
Martynova Elina <malina@MacBook-Pro-malina.local>
2026-03-13 10:18:33 +0300

    update DeadlineTask to support Priority enum

--- src/core/DeadlineTask.hpp
+++ src/core/DeadlineTask.hpp
@@ -1,15 +1,19 @@
-#ifndef DEADLINE_TASK_HPP
-#define DEADLINE_TASK_HPP
+#ifndef DEADLINETASK_HPP
+#define DEADLINETASK_HPP
+
 #include "Task.hpp"
 #include <string>
+
 class DeadlineTask : public Task {
 private:
     std::string deadline_;
+
 public:
-    DeadlineTask(int id, const std::string& title, const std::string& deadline);//Чтобы не копировать строку.Это быстрее.
+    DeadlineTask(int id, const std::string& title, const std::string& deadline, Priority priority);
+
     const std::string& getDeadline() const;
-    void setDeadline(const std::string& deadline);
     std::string getType() const override;
-    std::string toString() const override;//override - Я переопределяю виртуальную функцию из базового класса
+    std::string toString() const override;
 };
+
 #endif
